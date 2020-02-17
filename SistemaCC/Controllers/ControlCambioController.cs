dc0768cb59571b9171c5e850ec36dd652adbc844
AmeDaf <ame_rainbow@hotmail.com>
2020-02-16 20:50:37 -0600

    Campos dinamicos completos con contadores

--- SistemaCC/Controllers/ControlCambioController.cs
+++ SistemaCC/Controllers/ControlCambioController.cs
@@ -23,13 +23,13 @@ namespace SistemaCC.Controllers
         // GET: ControlCambio/Crear
         public ActionResult Crear()
         {
-            //var usuarios = (from a in BD.Usuario select a).ToList();
-            //List<Usuario> usuarios2 = new List<Usuario>();
-            //foreach (var usuario in usuarios)
-            //{
-            //    usuarios2.Add(new Usuario { Id_U = usuario.Id_U, Nombre = usuario.Nombre + usuario.ApePaterno + usuario.ApeMaterno });
-            //}
-            //ViewData["usuarios"] = new SelectList(usuarios2, "Id_U", "Nombre");
+            var usuarios = (from a in BD.Usuario select a).ToList();
+            List<Usuario> usuarios2 = new List<Usuario>();
+            foreach (var usuario in usuarios)
+            {
+                usuarios2.Add(new Usuario { Id_U = usuario.Id_U, Nombre = usuario.Nombre + usuario.ApePaterno + usuario.ApeMaterno });
+            }
+            ViewData["usuarios"] = new SelectList(usuarios2, "Id_U", "Nombre");
             return View(); 
         }
 
