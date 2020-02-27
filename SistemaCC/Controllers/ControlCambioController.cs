759f372d72f1bd5431e6cb867335282979c74895
AmeDaf <ame_rainbow@hotmail.com>
2020-02-27 01:47:02 -0600

    Backend crear control para actividades

--- SistemaCC/Controllers/ControlCambioController.cs
+++ SistemaCC/Controllers/ControlCambioController.cs
@@ -13,68 +13,7 @@ namespace SistemaCC.Controllers
     public class ControlCambioController : Controller
     {
         BDControlCambioDataContext BD = new BDControlCambioDataContext();
-        DateTime fecha(string fecha)
-        {
-            int dia, mes, anio;
-            dia = Convert.ToInt32(fecha.Substring(8, 2));
-            mes = Convert.ToInt32(fecha.Substring(5, 2));
-            anio = Convert.ToInt32(fecha.Substring(0, 4));
-            return new DateTime(anio, mes, dia);
-        }
-        //Funcion para dar formato a las actividades
-        List<Actividades> actividades(string[] act, string tipo)
-        { 
-            List<Actividades> actividades = new List<Actividades>();
-            string[] act_prev_desc = act[0].Split(new Char[] { '/', ',', '.', ':', '\t' });
-            string[] act_prev_obs = act[0].Split(new Char[] { '/', ',', '.', ':', '\t' });
-            string[] act_prev_fecha = act[0].Split(new Char[] { '/', ',', '.', ':', '\t' });
-            string[] act_prev_usuario = act[0].Split(new Char[] { '/', ',', '.', ':', '\t' });
-            for(var i = 0; i < act_prev_desc.Length; i++)
-            {
-                Actividades actividades_ = new Actividades();
-                actividades_.Descripcion = act_prev_desc[i];
-                actividades_.Observaciones = act_prev_obs[i];
-                actividades_.FechaRealizacion = fecha(act_prev_fecha[i]);
-                actividades_.Responsable = Convert.ToInt32(act_prev_usuario[i]);
-                actividades_.Tipo = tipo;
-                actividades.Add(actividades_);
-            }
-            return actividades;
-        }
-        //Funcion para insertar las actividades
-        void anadir_actividades(int id_CC, string[] act_prev, string[] act_cc)
-        {
-            if (!act_prev.Contains(null))
-            {
-                List<Actividades> act_prev_ = actividades(act_prev, "Previa");
-                foreach (var a in act_prev_)
-                {
-                    BD.Actividades.InsertOnSubmit(a);
-                    BD.SubmitChanges();
-                    //Se inserta en la tabla intermedia
-                    ActividadesControl ac = new ActividadesControl();
-                    ac.fk_CC = id_CC;
-                    ac.fk_Ac = a.Id_Ac;
-                    BD.ActividadesControl.InsertOnSubmit(ac);
-                    BD.SubmitChanges();
-                }
-            }
-            if (act_cc.Contains(null))
-            {
-                List<Actividades> act_cc_ = actividades(act_cc, "ControlCambio");
-                foreach (var a in act_cc_)
-                {
-                    BD.Actividades.InsertOnSubmit(a);
-                    BD.SubmitChanges();
-                    //Se inserta en la tabla intermedia
-                    ActividadesControl ac = new ActividadesControl();
-                    ac.fk_CC = id_CC;
-                    ac.fk_Ac = a.Id_Ac;
-                    BD.ActividadesControl.InsertOnSubmit(ac);
-                    BD.SubmitChanges();
-                }
-            }
-        }
+
         // GET: ControlCambio/Ver/5
         public ActionResult Ver(int id)
         {
@@ -102,34 +41,11 @@ namespace SistemaCC.Controllers
         {
             try
             {
-                //PRIMERO: Insertar en la tabla control de cambio
-                ControlCambio controlCambio = new ControlCambio();
-                controlCambio.Titulo = model.Titulo;
-                controlCambio.FechaEjecucion = model.FechaEjecucion;
-                controlCambio.Introduccion = model.Introduccion;
-                controlCambio.Objetivos = model.Objetivos;
-                controlCambio.Tipo = model.Tipo;
-                controlCambio.Estado = "Creado";
-                controlCambio.Creador = 1;
-                BD.ControlCambio.InsertOnSubmit(controlCambio);
-                BD.SubmitChanges();
-                //Llamar anadir actividades
-                string[] act_prev = new string[4];
-                string[] act_cc = new string[4];
-                act_prev[0] = collection["actividades_prev_descripcion"];
-                act_prev[1] = collection["actividades_prev_observaciones"];
-                act_prev[2] = collection["actividades_prev_fecha"];
-                act_prev[3] = collection["actividades_prev_usuarios"];
-                act_cc[0] = collection["actividades_cc_descripcion"];
-                act_cc[1] = collection["actividades_cc_observaciones"];
-                act_cc[2] = collection["actividades_cc_fecha"];
-                act_cc[3] = collection["actividades_cc_usuarios"];
-                anadir_actividades(controlCambio.Id_CC, act_prev, act_cc);
+                //TODO: Add insert logic here
                 return RedirectToAction("./../Home/Index");
             }
-            catch(System.Data.SqlClient.SqlException ex)
+            catch
             {
-                Console.Write(ex);
                 return View();
             }
         }
